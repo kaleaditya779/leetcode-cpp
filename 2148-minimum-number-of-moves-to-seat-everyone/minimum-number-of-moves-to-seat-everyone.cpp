0477c1c13479483dacab5e0c4f3b29f7760f860d
Aditya Kale <113056317+kaleaditya779@users.noreply.github.com>
2024-07-20 01:49:31 +0530

    Time: 22 ms (6.39%) | Memory: 24.2 MB (20.06%) - LeetSync

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        // In my opinion such problems are of greedy approach
+        // Means when iterating through students, we have to find the nearest (least moves) seats..
+        sort(students.begin(), students.end());
+        sort(seats.begin(), seats.end());
+        // unordered_map<int, int> studSeat;
+        vector<vector<int>> studSeat(students.size());
+        for (int i=0; i<students.size(); i++){
+            studSeat[i]={students[i], seats[i]};
+        }
+
+        for (auto i:studSeat){
+            cout<<i[0]<<": "<<i[1]<<endl; 
+        }
+
+        int minMoves=0;
+        for (auto i:studSeat){
+            if (i[0] == i[1]) continue;
+            else if (i[0] > i[1]){
+                minMoves+=(i[0]-i[1]);
+            } else if (i[0] < i[1]){
+                minMoves+=(i[1]-i[0]);
+            }
+        }
+        return minMoves;
+    }
+};
