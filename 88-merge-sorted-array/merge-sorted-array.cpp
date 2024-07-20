0232d43e04cf2de86c85a5d3fc33dc2bede8764c
Aditya Kale <113056317+kaleaditya779@users.noreply.github.com>
2024-07-21 01:31:11 +0530

    Time: 4 ms (28.99%) | Memory: 11.3 MB (11.74%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // sort(nums1.begin(), nums1.end());
+        // sort(nums2.begin(), nums2.end());
+        // int total = m+n;
+        vector<int> mergerBuddy;
+        int m1=0;
+        int m2=0;
+        while (m1<m && m2<n) {
+            if (nums1[m1]==nums2[m2]){
+                mergerBuddy.push_back(nums1[m1]);
+                mergerBuddy.push_back(nums1[m1]);
+                m1++; m2++;
+            } else if (nums1[m1]>nums2[m2]){
+                mergerBuddy.push_back(nums2[m2]);
+                m2++;
+            } else if (nums1[m1]<nums2[m2]){
+                mergerBuddy.push_back(nums1[m1]);
+                m1++;
+            }
+        }
+        while (m1+m2 < m+n){
+            if (m1 == m){
+                mergerBuddy.push_back(nums2[m2]);
+                m2++;
+            } else if (m2 == n){
+                mergerBuddy.push_back(nums1[m1]);
+                m1++;
+            }
+        }
+        // nums1.clear();
+        for (auto i:mergerBuddy){
+            cout<<i<<" ";
+        }
+        nums1 = mergerBuddy;
+    }
+};
