81bdda9f733deda155ca2b506109ea82e4362823
Aditya Kale <113056317+kaleaditya779@users.noreply.github.com>
2024-07-26 12:16:08 +0530

    Time: 97 ms (44.75%) | Memory: 73.4 MB (31.71%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // for (int i=0; i<nums.size(); i++){
-        //     int cnt = count(nums.begin(), nums.end(), nums[i]);
-        //     if (cnt > 1){
-        //         return true;
-        //         break;
-        //     } else {
-        //         continue;
-        //     }
-        // }
-        // return false;
+        // 3 approaches: O(N^2) space-O(1), O(NlogN) space-O(1), O(N) space-O(N);
 
+        // Comparing each to every other:
+        /*
+        for (int i=0; i<nums.size()-1; i++){
+            for (int j=i; j<nums.size()){
+                if (nums[i] == nums[j]) return true;
+                else return false;
+            }
+        }
+        */
+
+        // Sorting and comparing neghbours:
+        /*
         sort(nums.begin(), nums.end());
         for (int i=0; i<nums.size()-1; i++){
             if (nums[i] == nums[i+1]){
@@ -19,5 +22,15 @@ public:
             }
         }
         return false;
+        */
+
+        // Using set to store each encountering element, if already exists return true:
+        unordered_set<int> theSet;
+        for (int i=0; i<nums.size(); i++){
+            if (theSet.count(nums[i]) == 1) return true;
+            else theSet.insert(nums[i]); 
+        }
+        return false;
+        
     }
 };
