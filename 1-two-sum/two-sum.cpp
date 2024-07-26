95914c295f94a3fcc1a1d8c2ba6d2fa47761999b
Aditya Kale <113056317+kaleaditya779@users.noreply.github.com>
2024-07-26 18:23:24 +0530

    Time: 11 ms (63.38%) | Memory: 16 MB (5.90%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -9,25 +9,10 @@ public:
             }
         }
         */
-        // 2. sorting the array then using 2 pointers: O(NlogN + N) = O(NlogN), space = O(1)
-        /*
-        sort(nums.begin(), nums.end());
-        int left = 0;
-        int right = nums.size()-1;
-        while (left<right) {
-            if (nums[left]+nums[right] == target) return {left, right};
-            else if (nums[left]+nums[right] > target) right--;
-            else if (nums[left]+nums[right] < target) left++;
-        }
-        */
-        
-        // 3. Sorting the array and then applying binary search on array for 2nd index, considering 1st index as each point at a time: O(NlogN + NlogN) = O(NlogN), space = O(1)
 
-
-
-
-        // 4. Using hash tables 
+        // 2. Using hash tables 
         // setting map with val:index, then subtract i from target and if the given value is present in map, return their index.: O(N), space = O(N)
+        
         unordered_map<int, int> num;
         for (int i=0; i<nums.size(); i++){
             num[nums[i]]=i;
@@ -37,6 +22,26 @@ public:
             if (num.count(require) == 1 && num[require] != i) return {i, num[require]};
             else continue;
         }
-        return {3};
+        
+        
+        // ** IMP : All the approaches where sorting is there, they don't work, as the indices of elements get changed..
+        // 3. sorting the array then using 2 pointers: O(NlogN + N) = O(NlogN), space = O(1) 
+        // sort(nums.begin(), nums.end());
+        // int left = 0;
+        // int right = nums.size()-1;
+        // while (left<right) {
+        //     if (nums[left]+nums[right] == target) return {left, right};
+        //     else if (nums[left]+nums[right] > target) right--;
+        //     else if (nums[left]+nums[right] < target) left++;
+        // }
+        
+        
+        
+
+
+
+
+
+        return {};
     }
 };
