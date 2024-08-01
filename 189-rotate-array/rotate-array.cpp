8ad26c659cbdf7bbba8ade9629e67ae7b762fee6
Aditya Kale <113056317+kaleaditya779@users.noreply.github.com>
2024-08-01 18:55:55 +0530

    Time: 21 ms (54.84%) | Memory: 27.3 MB (70.91%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -5,12 +5,43 @@ public:
         // ex. [1,2,3,4] 
         // rotate by 2 => [3,4,1,2]
         int n = nums.size();
+        /*
         // approach 1:
         vector<int> dupli(n);
         for (int i=0; i<n; i++){
             dupli[(i+k)%n]=nums[i];
         }
-        nums=dupli;
+        nums=dupli; */
+
+        // approach 2:
+        // reverse the input array. then reverse 1st k ele. then reverse remaining eles..
+        int l=0, r=n-1;
+        while (l<r){
+            int temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
+            l++;
+            r--;
+        }
+        // now reverse 1st k eles,
+        l=0, r=(k%n)-1;
+        while (l<r){
+            int temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
+            l++;
+            r--;
+        }
+        // reverse the rest
+        l=(k%n), r=n-1;
+        while (l<r){
+            int temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
+            l++;
+            r--;
+        }
+
 
     }
 };
