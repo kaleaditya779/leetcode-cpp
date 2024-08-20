de2dd6f811949f133a2bb8a15236659e26488520
Aditya Kale <113056317+kaleaditya779@users.noreply.github.com>
2024-08-20 09:14:47 +0530

    Time: 8 ms (27.43%) | Memory: 15.3 MB (5.62%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,65 +17,25 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        
-        if (head==NULL) {
-            return NULL;
-        }
-
-
-        Node* h = new Node(head->val);
-        Node* iter = new Node(head->val); // iter to original ll
-        iter = head;
-        iter=iter->next;
-
-        Node* pre = new Node(h->val); // pre to duplicate ll
-        pre=h;
+        unordered_map<Node*, Node*> uMap;
+        // Basically what i what to do is that store original ll as key and duplicate as value..
+        Node* iter = head;
         while (iter!=NULL){
             Node* temp = new Node(iter->val);
-            if (pre->next==NULL) pre->next=temp;
-            pre=pre->next;
+            uMap[iter] = temp;
             iter=iter->next;
         }
-        
-        iter=head; // bringing the iter back to head
-        int index=0;
-        while (iter != NULL) {
-            iter->val=index;
-            iter=iter->next;
-            index++;
-        }
-        
-        unordered_map<int, int> indexRand;
+
+        // now what is remaining is to give next and random links to duplicate ll.
         iter=head;
-        for (int i=0; i<index; i++){
-            if (iter->random == NULL) {
-                indexRand[iter->val] = -1;
-            }
-            else {
-                indexRand[iter->val] = iter->random->val;
-            }
+        Node* temp = uMap[iter];
+        while (iter!=NULL){
+            uMap[iter]->next = uMap[iter->next];
+            uMap[iter]->random = uMap[iter->random];
+            // understand above 2 lines and that's all !!!!!
             iter=iter->next;
         }
-
-        // for (auto i:indexRand) cout<<endl<<i.first<<" : "<<i.second;
-
-        pre = h;
-        Node* preIter = new Node(h->val);
-        preIter = h;
-        for (int i=0; i<index; i++){
-            if (indexRand[i]==-1) {
-                pre->random = NULL;
-                pre=pre->next;
-            } else {
-                for (int j=0; j<indexRand[i]; j++){
-                    preIter=preIter->next;
-                }
-                pre->random=preIter;
-                preIter=h;
-                pre=pre->next;
-            }
-        }
-
-        return h;
+        iter=head;
+        return uMap[iter];
     }
 };
